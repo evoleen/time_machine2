9ce0e37642d90f406ab332eb6b62781983674cc8
Till Gerken <till.gerken@gmail.com>
2025-01-06 17:24:01 +0100

    Fix tests and broken code

--- lib/src/text/globalization/culture.dart
+++ lib/src/text/globalization/culture.dart
@@ -7,31 +7,31 @@ import 'package:time_machine/src/time_machine_internal.dart';
 
 @internal
 abstract class ICultures {
-  static set currentCulture(Culture value) { Cultures._currentCulture = value; }
-  static void loadAllCulturesInformation_SetFlag() {
-    if (Cultures._loader != null) throw StateError('loadAllCultures flag may not be set after Cultures are initalized.');
-    Cultures._loadAllCulturesInformation = true;
+  static set currentCulture(Culture value) {
+    Cultures._currentCulture = value;
   }
 
-  static bool get allCulturesLoaded => Cultures._loadAllCulturesInformation;
+  static void loadAllCulturesInformation_SetFlag() {
+    if (Cultures._loader != null)
+      throw StateError(
+          'loadAllCultures flag may not be set after Cultures are initalized.');
+  }
 }
 
 abstract class Cultures {
-  // todo: this is a bandaid ~ we need to rework our infrastructure a bit -- maybe draw some diagrams?
-  // This gives us the JS functionality of just minimizing our timezones, and it gives us the VM/Flutter functionality of just loading them all from one file.
-  static bool _loadAllCulturesInformation = false;
-
   static CultureLoader? _loader;
-  static Future<CultureLoader> get _cultures async => _loader??= await (_loadAllCulturesInformation ? CultureLoader.loadAll() : CultureLoader.load());
+  static Future<CultureLoader> get _cultures async =>
+      _loader ??= await CultureLoader.loadAll();
 
   static Future<Iterable<String>> get ids async => (await _cultures).cultureIds;
-  static Future<Culture?> getCulture(String id) async => (await _cultures).getCulture(id);
+  static Future<Culture?> getCulture(String id) async =>
+      (await _cultures).getCulture(id);
 
   static final Culture invariantCulture = Culture._invariant();
 
   // todo: we need a way to set this for testing && be able to set this with Platform Initialization (and have it not be changed at random)
   static Culture? _currentCulture;
-  static Culture get currentCulture => _currentCulture??=invariantCulture;
+  static Culture get currentCulture => _currentCulture ??= invariantCulture;
 }
 
 // todo: look to combine this with TimeMachineInfo and we can merge all the *_pattern.create*() functions!
@@ -40,8 +40,10 @@ class Culture {
   static final Culture invariant = Culture._invariant();
 
   static Culture? _current;
-  static Culture get current => _current??=invariant;
-  static set current(Culture value) { _current = value; }
+  static Culture get current => _current ??= invariant;
+  static set current(Culture value) {
+    _current = value;
+  }
 
   bool get isReadOnly => true;
 
@@ -55,10 +57,10 @@ class Culture {
   Culture._invariant()
       : dateTimeFormat = DateTimeFormatBuilder.invariant().Build(),
         name = invariantId,
-        compareInfo = null
-  ;
+        compareInfo = null;
 
   const Culture(this.name, this.dateTimeFormat) : compareInfo = null;
 
-  @override String toString() => name;
+  @override
+  String toString() => name;
 }
