07fd02c9c778438ba07f7df09d180348161136a3
Till Gerken <till.gerken@gmail.com>
2025-01-06 17:48:11 +0100

    Fix asset loader

--- lib/src/platforms/web.dart
+++ lib/src/platforms/web.dart
@@ -74,7 +74,7 @@ class _WebMachineIO implements PlatformIO {
     // // todo: probably a better way to do this
     // var binary = new ByteData.view(new Int8List.fromList(await resource.readAsBytes()).buffer);
 
-    var resource = Uri.parse('packages/time_machine/data/$path/$filename');
+    var resource = Uri.parse('packages/time_machine2/data/$path/$filename');
     var binary =
         ByteData.view(Int8List.fromList(await _readAsBytes(resource)).buffer);
 
@@ -87,7 +87,7 @@ class _WebMachineIO implements PlatformIO {
     // var resource = new Resource('packages/time_machine/data/$path/$filename');
     // return json.decode(await resource.readAsString());
 
-    var resource = Uri.parse('packages/time_machine/data/$path/$filename');
+    var resource = Uri.parse('packages/time_machine2/data/$path/$filename');
     return json.decode(await _readAsString(_resolveUri(resource), null));
   }
 }
