9e538c649d22efd70da6f35af0ab213dddc00fbf
tigloo <Till.gerken@gmail.com>
2025-01-08 21:51:44 +0100

    Merge text patterns into main include (#27)

--- lib/time_machine2.dart
+++ lib/time_machine2.dart
@@ -86,6 +86,27 @@ export 'src/timezones/invalid_datetimezone_source_error.dart'
     show InvalidDateTimeZoneSourceError;
 export 'src/timezones/resolvers.dart' show Resolvers;
 
+// Text and Parsing
+export 'src/text/composite_pattern_builder.dart' show CompositePatternBuilder;
+export 'src/text/i_pattern.dart' show IPattern;
+export 'src/text/parse_result.dart' show ParseResult;
+export 'src/text/unparsable_value_error.dart' show UnparsableValueError;
+export 'src/text/period_pattern.dart' show PeriodPattern;
+export 'src/text/invalid_pattern_error.dart' show InvalidPatternError;
+
+export 'src/text/localdate_pattern.dart' show LocalDatePattern;
+export 'src/text/localtime_pattern.dart' show LocalTimePattern;
+export 'src/text/localdatetime_pattern.dart' show LocalDateTimePattern;
+
+export 'src/text/annual_date_pattern.dart' show AnnualDatePattern;
+export 'src/text/duration_pattern.dart' show TimePattern;
+export 'src/text/instant_pattern.dart' show InstantPattern;
+export 'src/text/offset_date_pattern.dart' show OffsetDatePattern;
+export 'src/text/offset_datetime_pattern.dart' show OffsetDateTimePattern;
+export 'src/text/offset_pattern.dart' show OffsetPattern;
+export 'src/text/offset_time_pattern.dart' show OffsetTimePattern;
+export 'src/text/zoneddatetime_pattern.dart' show ZonedDateTimePattern;
+
 // These are not used (but aren't @internal either)
 // export 'src/timezones/tzdb_zone_1970_location.dart';
 // export 'src/timezones/tzdb_zone_location.dart';
