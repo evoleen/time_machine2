4dda953f747cae4c815b885fcb8561c96e2a2fb3
Till Gerken <till.gerken@gmail.com>
2025-01-14 11:21:52 +0100

    Fix incorrect attribution

--- lib/src/timezones/tzdb_datetimezone_source_browser_impl.dart
+++ lib/src/timezones/tzdb_datetimezone_source_browser_impl.dart
@@ -1,7 +1,3 @@
-// Copyright (c) 2014, the timezone project authors. Please see the AUTHORS
-// file for details. All rights reserved. Use of this source code is governed
-// by a BSD-style license that can be found in the LICENSE file.
-
 import 'package:archive/archive.dart';
 import 'package:time_machine2/src/platforms/platform_io.dart';
 
