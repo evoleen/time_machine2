a9f3d4416a5b2b466ad84491b8c492151c112aef
Till Gerken <till.gerken@gmail.com>
2025-01-14 16:00:28 +0100

    Remove unneeded discovery mechanism

--- lib/src/platforms/flutter_discovery/flutter_discovery.dart
+++
@@ -1,5 +0,0 @@
-/// Platform discovery method that exposes a boolean [kIsFlutter].
-/// Will be set to true if compiled for Flutter, set to false when compiled
-/// for Dart only.
-
-export 'dart_pure.dart' if (dart.library.ui) 'dart_flutter.dart';
